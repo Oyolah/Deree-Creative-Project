80a48b0b105e1d01046101d69f2f772a4b1752fa
Oyolah <53017008+Oyolah@users.noreply.github.com>
2025-04-11 21:37:04 +0300

    add backend url

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(
     cors({
-        origin: ["http://localhost:5173"],
+        origin: ["http://localhost:5173", "https://one7roots.onrender.com"],
         credentials: true,
     })
 );
